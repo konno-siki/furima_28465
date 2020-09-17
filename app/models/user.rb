741285b5a6a20fdf807127633805a1ffc66433f2
konno-siki <sakurakanji@icloud.com>
2020-09-17 18:31:51 +0900

    正規表現の変数化

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  full_size_japanese = /\A[ぁ-んァ-ンー-龥]/
+  full_width_kana = /\A[ァ-ヶーー]+\z/
   with_options presence: true do
-    validates :first_name, format: { with: /\A[ぁ-んァ-ンー-龥]/, message: 'is invalid. Input full-width characters.' }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ンー-龥]/, message: 'is invalid. Input full-width characters.' }
-    validates :first_name_reading, format: { with: /\A[ァ-ヶーー]+\z/, message: 'is invalid. Input full-width katakana characters.' }
-    validates :last_name_reading, format: { with: /\A[ァ-ヶーー]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+    validates :first_name, format: { with: full_size_japanese, message: 'is invalid. Input full-width characters.' }
+    validates :last_name, format: { with: full_size_japanese, message: 'is invalid. Input full-width characters.' }
+    validates :first_name_reading, format: { with: full_width_kana, message: 'is invalid. Input full-width katakana characters.' }
+    validates :last_name_reading, format: { with: full_width_kana, message: 'is invalid. Input full-width katakana characters.' }
     validates :nickname
     validates :birthday
     validates :password, format: { with: /\A(?=.*?[A-Za-z])(?=.*?[0-9])[a-zA-Z0-9]+\z/, message: 'is invalid. Input half-width alphanumeric characters and numerical characters.' }, length: { minimum: 6 }, confirmation: true
