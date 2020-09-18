dedf0567a1cd419a8d0e54b88a82a838ac01ba7c
konno-siki <sakurakanji@icloud.com>
2020-09-18 12:09:30 +0900

    ログインユーザーのみ出品ページへ遷移実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,17 @@
 class ItemsController < ApplicationController
+  before_action :move_to_new, except: [:index]
+    
   def index
+  end
+
+  def new
+  end
+
+  private
+
+  def move_to_new
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
