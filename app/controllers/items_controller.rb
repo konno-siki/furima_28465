68475214373f63e9658078a08b614cc70a2ceae3
konno-siki <sakurakanji@icloud.com>
2020-09-23 20:48:35 +0900

    出品者、出品者以外のリンク制限実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_new, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @items = Item.includes(:user).order(created_at: :desc)
@@ -28,7 +28,7 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :item_name, :text, :category_id, :status_id, :fee_id, :prefecture_id, :delivery_id, :price).merge(user_id: current_user.id)
   end
 
-  def move_to_new
+  def move_to_index
     redirect_to action: :index unless user_signed_in?
   end
 end
