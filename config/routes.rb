03602359b220a65d4b9fa7543afbd318d1a6be94
konno-siki <sakurakanji@icloud.com>
2020-09-28 17:08:19 +0900

    gem導入、MVC設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'items#index'
   resources :items
-  resources :orders, only: :index
+  resources :orders, only: [:index, :create]
   devise_for :users
 end
